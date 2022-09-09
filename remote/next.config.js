6d69f9d9baaab59534e3a93953b9ac60559d98e8
Joey Gough <j.gough@novafutur.com>
2022-09-09 14:58:43 +0100

    add module federation config to remote

--- remote/next.config.js
+++ remote/next.config.js
@@ -1,7 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const NextFederationPlugin = require("@module-federation/nextjs-mf/lib/NextFederationPlugin");
 
-module.exports = nextConfig
+module.exports = {
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "remote",
+          remotes: {
+            // next1: `host@http://localhost:3000/_next/static/chunks/remoteEntry.js`,
+          },
+          filename: "static/chunks/remoteEntry.js",
+          exposes: {
+            "./Button": "./components/Button.tsx",
+          },
+          shared: {
+            // whatever else
+          },
+        })
+      );
+    }
+
+    return config;
+  },
+};
