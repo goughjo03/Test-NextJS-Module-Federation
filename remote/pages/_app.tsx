008b9f395f540d175bbb6e30e540ef2268f42f65
Joey Gough <j.gough@novafutur.com>
2022-09-09 15:00:46 +0100

    add package import to avoid tree shaking problems

--- remote/pages/_app.tsx
+++ remote/pages/_app.tsx
@@ -1,6 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// _app.js or some other file in as high up in the app (like next's new layouts)
+// this ensures various parts of next.js are imported and "used" somewhere so that they wont be tree shaken out
+import '@module-federation/nextjs-mf/lib/include-defaults';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
